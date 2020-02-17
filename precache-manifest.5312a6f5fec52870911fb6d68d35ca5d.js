da8bcd6aacf765e6d1fd0c54920b0a715276aed7
Matt Pignatore <mattpigs@gmail.com>
2020-02-17 07:47:29 -0500

    Updates

---
+++ precache-manifest.5312a6f5fec52870911fb6d68d35ca5d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc5804c97683dc524f3a91a2700b5f8",
+    "url": "/the-floor-is-yours/index.html"
+  },
+  {
+    "revision": "48964f4d388ff1677951",
+    "url": "/the-floor-is-yours/static/css/main.661afc07.chunk.css"
+  },
+  {
+    "revision": "a88b707e832f1c49cac7",
+    "url": "/the-floor-is-yours/static/js/2.4cbd95d1.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/the-floor-is-yours/static/js/2.4cbd95d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48964f4d388ff1677951",
+    "url": "/the-floor-is-yours/static/js/main.2d5f994d.chunk.js"
+  },
+  {
+    "revision": "3f19959de25793edb674",
+    "url": "/the-floor-is-yours/static/js/runtime-main.665db34d.js"
+  },
+  {
+    "revision": "f0777fc3eacba637b01665fb51bf4001",
+    "url": "/the-floor-is-yours/static/media/cover-image-1500.f0777fc3.png"
+  },
+  {
+    "revision": "5eec348155538ef9543f66fd4d3d971c",
+    "url": "/the-floor-is-yours/static/media/floor-is-yours-logo.5eec3481.png"
+  },
+  {
+    "revision": "f027d73e41e42b79b3866d0076a2e586",
+    "url": "/the-floor-is-yours/static/media/hip-hop-image-1500.f027d73e.png"
+  },
+  {
+    "revision": "3b7abe4938b3f4b757c6543dc290ca68",
+    "url": "/the-floor-is-yours/static/media/logo-2.3b7abe49.png"
+  },
+  {
+    "revision": "a7bb5ab28a4144c6466009b84a166d73",
+    "url": "/the-floor-is-yours/static/media/pole-image-1500.a7bb5ab2.png"
+  },
+  {
+    "revision": "2f8d88309922a855c5ee58d5404a41c1",
+    "url": "/the-floor-is-yours/static/media/salsa-image-1500.2f8d8830.png"
+  },
+  {
+    "revision": "4208d8782298da760a058cde0e064e3a",
+    "url": "/the-floor-is-yours/static/media/silks-image-1500.4208d878.png"
+  },
+  {
+    "revision": "614b4e782332f0f9f097e0d8cbf27a19",
+    "url": "/the-floor-is-yours/static/media/yoga-image-1500.614b4e78.png"
+  }
+]);
